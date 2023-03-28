bb8528ff651cc8a2d4d5b1ddf6ea1253a2319079
george <georgev421@gmail.com>
2023-03-28 11:29:06 +0100

    fixed task 3 eslint bug

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(studentsList) {
-    return studentsList.reduce((acc, student) => acc + student.id, 0);
+  return studentsList.reduce((acc, student) => acc + student.id, 0);
 }
